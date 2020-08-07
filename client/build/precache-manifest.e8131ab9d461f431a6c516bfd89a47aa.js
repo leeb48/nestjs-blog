0b26964ab47e7ba2d345565ed79afd712c056047
Bong Lee <bongster228@github.com>
2020-08-07 00:45:32 -0700

    backend dev environment setup with containers

---
+++ client/build/precache-manifest.e8131ab9d461f431a6c516bfd89a47aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8a161753ce9167a1b909dcf6991ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d275cf43d591c05b88b",
+    "url": "/static/css/main.7a743ae7.chunk.css"
+  },
+  {
+    "revision": "bc6bd15b44637e36db43",
+    "url": "/static/js/2.e207116c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e207116c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d275cf43d591c05b88b",
+    "url": "/static/js/main.45258728.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6dd9cb774b05b36d10caf7c3e7cd6fe8",
+    "url": "/static/media/landing.6dd9cb77.jpg"
+  }
+]);
